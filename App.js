5aa25a64bb8829fc4ddc54d3e1ccb78c1327327e
Erzender <lukas.parant@epitech.eu>
2018-11-02 15:25:54 +0100

    redux

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './App/reducer';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,8 +14,12 @@ const styles = StyleSheet.create({
   },
 });
 
+const store = createStore(rootReducer);
+
 export default () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-  </View>
+  <Provider store={store}>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  </Provider>
 );
---
+++ App/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const root = (state = initialState, action) => {
+  switch (action.ref) {
+    default:
+      return state;
+  }
+};
+
+export default root;
